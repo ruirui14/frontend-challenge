3c9b364fa00133df528369d4ed71ed9d10cd502d
ruirui14 <2230103@ecc.ac.jp>
2025-10-27 09:47:22 +0900

    エンドポイントGET

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,39 @@
-//import { useState } from 'react'
 //import './App.css'
+import { useEffect,useState } from "react";
 
+const API_URL = import.meta.env.VITE_API_BASE_URL;
+const API_KEY = import.meta.env.VITE_API_KEY;
 
 function App() {
   const items = Array.from({ length: 40 }, (_, i) => `項目 ${i + 1}`);
+  const [data,setData] = useState<any[]>([]);
+  const [error,setError] = useState<string | null>(null);
+  const [loading,setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/api/api/v1/prefectures", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json; charset=UTF-8",
+            "X-API-KEY": API_KEY,
+          },
+        });
+
+        if(!response.ok) throw new Error(`Error: ${response.status}`)
+
+        const result = await response.json();
+        setData(result);  
+      } catch ( error:any) {
+          setError(error.message);
+      } finally {
+          setLoading(false);
+      }  
+      };
+      fetchData();
+  },[]);
+
   return (
     <>  
         <title>都道府県別総人口推移グラフ</title>
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://frontend-engineer-codecheck-api.mirai.yumemi.io/",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 })
