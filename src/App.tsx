1c0436e88c20fd8f030c175a4ce123f02bf90f53
ruirui14 <2230103@ecc.ac.jp>
2025-10-27 10:31:16 +0900

    人口構成データ取得・チェックボックスと連動

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 //import './App.css'
 import { useEffect,useState } from "react";
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
 
-// const API_URL = import.meta.env.VITE_API_BASE_URL;
+//const API_URL = import.meta.env.VITE_API_BASE_URL;
 const API_KEY = import.meta.env.VITE_API_KEY;
 
 function App() {
   //const items = Array.from({ length: 40 }, (_, i) => `項目 ${i + 1}`);
   const [datas,setData] = useState<any[]>([]);
   const [error,setError] = useState<string | null>(null);
+  const [selectPref, setSelectPref] = useState<number[]>([]);
+  const [populationData, setPopulationData] = useState<any[]>([]);
 
+  //都道府県一覧を取得
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -34,6 +39,40 @@ function App() {
       fetchData();
   },[]);
 
+  const handleCheckBox = async (prefCode: number, checked: boolean) => {
+    if (checked) {
+      setSelectPref((prev) => [...prev, prefCode]);
+      await fetchPopulation(prefCode);
+    }else{
+      setSelectPref((prev) => prev.filter((code) => code !== prefCode));
+      setPopulationData((prev) => prev.filter((p) => p.prefCode !== prefCode));
+    }
+  };
+
+  //人口構成取得
+  const fetchPopulation = async (prefCode: number) => {
+    try{
+      const response = await fetch(`api/api/v1/population/composition/perYear?prefCode=${prefCode}`,
+        {
+          method: "GET",
+          headers: {
+            "X-API-KEY": API_KEY,
+            "Content-Type": "application/json; charset=UTF-8",
+          },
+        }
+      );
+      const result = await response.json();
+      console.log(`人口構成 (${prefCode})`, result.result.data);
+      setPopulationData((prev) => [
+        ...prev,
+        {prefCode, data: result.result.data},
+      ])
+    }catch (error){
+      console.error("人口構成取得エラー",error);
+    }
+  };
+
+
   return (
     <>  
         <title>都道府県別総人口推移グラフ</title>
@@ -46,7 +85,11 @@ function App() {
           <div>
             {datas.map((data) => (
               <label key={data.prefCode} >
-                <input type="checkbox" id={`data-${data.prefCode}`} name={`${data.prefName}`} />
+                <input type="checkbox" 
+                id={`data-${data.prefCode}`} 
+                name={`${data.prefName}`} 
+                onChange={(e) => handleCheckBox(data.prefCode, e.target.checked)}
+                />
                 {data.prefName}
               </label>
             ))}
