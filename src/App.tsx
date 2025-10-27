a8212a910c231b24e26858f4d08704798b7c0eb5
ruirui14 <2230103@ecc.ac.jp>
2025-10-27 09:22:16 +0900

    40個のチェックボックス

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+//import { useState } from 'react'
+//import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const items = Array.from({ length: 40 }, (_, i) => `項目 ${i + 1}`);
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <>  
+        <title>都道府県別総人口推移グラフ</title>
+        <div>
+          <h1>都道府県別総人口推移グラフ</h1>
+        </div>
+
+        <div>
+          <div>
+            <h2>都道府県</h2>
+          </div>
+
+          <div>
+            {items.map((item) => (
+              <label key={item} >
+                <input type="checkbox" id="hokka" name="hokka" />
+                北海道
+              </label>
+            ))}
+            
+          </div>
+        </div>
     </>
   )
 }
